caafb4fcd79d0b31bebb55161069e759e276b369
AJ Murray <aj.murr4y@gmail.com>
2023-01-30 14:51:02 -0500

    added validation to forms on login and register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,24 @@
 import '../App.css';
-import { Card, CardBody, CardFooter, CardHeader, CardTitle, CardText, Button, Input, Label } from 'reactstrap'
+import { Card, CardBody, CardFooter, CardHeader, CardTitle, CardText, Button, Input, Label, FormGroup, Form, FormFeedback } from 'reactstrap'
 import { useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 
 const Login = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [usernameTouched, setUsernameTouched] = useState(false);
+    const [passwordTouched, setPasswordTouched] = useState(false);
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        console.log(event)
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+    };
+
 
     return (
         <div className="login">
@@ -17,18 +30,45 @@ const Login = () => {
                         Log In
                     </CardTitle>
                     <CardText className='inputs'>
-                        <Label for='username' >Username</Label>
-                        <Input name='username' type='text' onChange={(e) => setUsername(e.target.value)} placeholder='Enter a username' />
-                        <Label for='password'>Password</Label>
-                        <Input name='password' type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Enter your password' />
+                        <Form name='login-form' id='login-form' noValidate validated={validated} onSubmit={handleSubmit}>
+                            <FormGroup>
+                                <Label for='username' >Username</Label>
+                                <Input
+                                    type='text'
+                                    name="username"
+                                    id="username"
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    placeholder='Enter a username'
+                                    required
+                                    value={username}
+                                    onBlur={() => setUsernameTouched(true)}
+                                    invalid={username.length == 0 && usernameTouched}
+                                />
+                                <FormFeedback invalid>Oops! You cannot leve username blank.</FormFeedback>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='password'>Password</Label>
+                                <Input
+                                    name='password'
+                                    id='password'
+                                    onBlur={() => setPasswordTouched(true)}
+                                    type='password'
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder='Enter your password'
+                                    required
+                                    value={password}
+                                    invalid={password.length == 0 && passwordTouched}
+                                />
+                                <FormFeedback invalid>Oops! You cannot leve password blank.</FormFeedback>
+                            </FormGroup>
+                        </Form>
                         <section id='link-container'>
                             <Link to='/register'>Register Account</Link>
-                            {/* <Link to='/forgotUsername'>Forgot Username</Link> */}
                         </section>
                     </CardText>
                 </CardBody>
                 <CardFooter>
-                    <Button color='primary'>
+                    <Button form='login-form' color='primary' type='submit'>
                         Submit
                     </Button>
                 </CardFooter>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,25 @@
-import { Link} from 'react-router-dom';
-import { Card, CardBody, CardFooter, CardHeader, CardTitle, CardText, Button, Input, Label } from 'reactstrap'
+import { Link } from 'react-router-dom';
+import { Card, CardBody, CardFooter, CardHeader, CardTitle, CardText, Button, Input, Label, FormFeedback, FormGroup, Form } from 'reactstrap'
 import { useState } from 'react';
 
 const Register = () => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const [confirmationPassword, setConfirmationPassword] = useState('')
+    const [confirmationPassword, setConfirmationPassword] = useState(null)
+    const [validated, setValidated] = useState(false);
+    const [usernameTouched, setUsernameTouched] = useState(false);
+    const [passwordTouched, setPasswordTouched] = useState(false);
+    const [confirmPasswordTouched, setConfirmPasswordTouched] = useState(false);
+
+    const handleSubmit = (event) => {
+        console.log(event)
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        setValidated(true)
+    };
     return (
         <div>
             <Card
@@ -16,20 +30,66 @@ const Register = () => {
                         Register
                     </CardTitle>
                     <CardText className='inputs'>
-                        <Label for='username' >Username</Label>
-                        <Input name='username' type='text'  placeholder='Enter a username'  onChange={(e) => setUsername(e.target.value)} value={username}/>
-                        <Label for='password'>Password</Label>
-                        <Input name='password' type='password'  placeholder='Enter your password' />
-                        <Label for='confirmpassword'>Confirm Password</Label>
-                        <Input name='confirmpassword' type='password'  placeholder='Renter your password' />
+                        <Form name='register-form' id='register-form' noValidate onSubmit={handleSubmit}>
+                            <FormGroup>
+                                <Label for='username' >Username</Label>
+                                <Input
+                                    name='username'
+                                    id='username'
+                                    type='text'
+                                    placeholder='Enter a username'
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    value={username}
+                                    onBlur={() => setUsernameTouched(true)}
+                                    valid={username.length > 0 && usernameTouched}
+                                    invalid={username.length == 0 && usernameTouched}
+                                />
+                                <FormFeedback valid>Username is valid</FormFeedback>
+                                <FormFeedback invalid>Oops! You need to enter a username.</FormFeedback>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='password'>Password</Label>
+                                <Input
+                                    name='password'
+                                    id='password'
+                                    type='password'
+                                    placeholder='Enter your password'
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    value={password}
+                                    onBlur={() => setPasswordTouched(true)}
+                                    minLength={8}
+                                    valid={password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/) && passwordTouched}
+                                    invalid={!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/) && passwordTouched}
+                                />
+                                <FormFeedback valid>Password meets all requirements.</FormFeedback>
+                                <FormFeedback invalid>Oops! You need to meet all password requirements.</FormFeedback>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for='confirmpassword'>Confirm Password</Label>
+                                <Input
+                                    name='confirmPassword'
+                                    id='confirmPassword'
+                                    type='password'
+                                    value={confirmationPassword}
+                                    onChange={(e) => setConfirmationPassword(e.target.value)}
+                                    placeholder='Renter your password'
+                                    required
+                                    onBlur={() => setConfirmPasswordTouched(true)}
+                                    minLength={8}
+                                    valid={password.match(confirmationPassword) && confirmPasswordTouched}
+                                    invalid={!password.match(confirmationPassword) && confirmPasswordTouched}
+                                />
+                                <FormFeedback valid>Passwords match you are all good</FormFeedback>
+                                <FormFeedback invalid>Oops! You need to fix the passwords to match.</FormFeedback>
+                            </FormGroup>
+                        </Form>
                         <section id='link-container'>
                             <Link to='/'>Login</Link>
-                            {/* <Link to='/forgotUsername'>Forgot Username</Link> */}
                         </section>
                     </CardText>
                 </CardBody>
                 <CardFooter>
-                    <Button color='primary'>
+                    <Button form='register-form' color='primary'>
                         Submit
                     </Button>
                 </CardFooter>
